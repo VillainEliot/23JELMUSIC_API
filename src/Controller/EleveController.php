1c88b71de8861fa5b484fc1d55b902efc7fbdf48
HRVNoa <hervieu.noa@gmail.com>
2024-04-05 10:47:13 +0200

    lister/consulter/Ajouter eleve en json

--- src/Controller/EleveController.php
+++ src/Controller/EleveController.php
@@ -8,8 +8,10 @@ use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\Persistence\ManagerRegistry;
 use Proxies\__CG__\App\Entity\Eleve;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Serializer\SerializerInterface;
 
 class EleveController extends AbstractController
 {
@@ -21,49 +23,73 @@ class EleveController extends AbstractController
         ]);
     }
 
-    public function listerEleve(ManagerRegistry $doctrine){
+    public function listerEleve(ManagerRegistry $doctrine, SerializerInterface $serializer){
 
         $repository = $doctrine->getRepository(Eleve::class);
 
         $eleves= $repository->findAll();
-        return $this->render('eleve/lister.html.twig', [
-            'pEleve' => $eleves,]);
+
+        $serializerController = new Serializer($serializer);
+        $ignAttr = ['eleves', 'eleve', 'cours','instruments', 'interventions', 'typeInstruments', 'interPrets', 'user'];
+        return $serializerController->serializeObject($eleves, $ignAttr);
+
+//        return $this->render('eleve/lister.html.twig', [
+//            'pEleve' => $eleves,]);
 
     }
 
-    public function consulterEleve(ManagerRegistry $doctrine, int $id){
+    public function consulterEleve(ManagerRegistry $doctrine, int $id, SerializerInterface $serializer){
 
         $eleve= $doctrine->getRepository(Eleve::class)->find($id);
 
         if (!$eleve) {
-            throw $this->createNotFoundException(
-                'Aucun eleve trouvé avec le numéro '.$id
-            );
+            return new JsonResponse(['error'=> true, 'message'=> 'L\'élève ne peut pas être consulté puisqu\'il n\'existe pas.']);
         }
 
+        $serializerController = new Serializer($serializer);
+        $ignAttr = ['eleves', 'eleve', 'cours','instruments', 'interventions', 'typeInstruments', 'interPrets', 'user'];
+        return $serializerController->serializeObject($eleve, $ignAttr);
         //return new Response('Eleve : '.$eleve->getNom());
-        return $this->render('eleve/consulter.html.twig', [
-            'eleve' => $eleve,]);
+//        return $this->render('eleve/consulter.html.twig', [
+//            'eleve' => $eleve,]);
     }
 
-    public function ajouterEleve(Request $request, EntityManagerInterface $entityManager): Response
+    public function ajouterEleve(Request $request, ManagerRegistry $doctrine, SerializerInterface $serializer): Response
     {
+        // récupération des données
+        $donnees = [
+            'nom'=> $request->get('nom'),
+            'prenom'=> $request->get('prenom'),
+            'num_rue'=> $request->get('num_rue'),
+            'rue'=> $request->get('rue'),
+            'copos'=> $request->get('copos'),
+            'ville'=> $request->get('ville'),
+            'tel'=> $request->get('tel'),
+            'mail'=> $request->get('mail'),
+        ];
         $eleve = new Eleve();
-        $form = $this->createForm(EleveType::class, $eleve);
 
-        $form->handleRequest($request);
 
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager->persist($eleve);
-            $entityManager->flush();
+        $eleve->setNom($donnees['nom']);
+        $eleve->setPrenom($donnees['prenom']);
+        $eleve->setNumRue($donnees['num_rue']);
+        $eleve->setRue($donnees['rue']);
+        $eleve->setCopos($donnees['copos']);
+        $eleve->setVille($donnees['ville']);
+        $eleve->setTel($donnees['tel']);
+        $eleve->setMail($donnees['mail']);
 
-            $this->addFlash('success', 'Eleve créé avec succès!');
-            return $this->redirectToRoute('eleveLister');
-        }
+        // insertion en base
+        $entityManager = $doctrine->getManager();
+        $entityManager->persist($eleve);
+        $entityManager->flush();
 
-        return $this->render('eleve/ajouter.html.twig', [
-            'form' => $form->createView(),
-        ]);
+        $serializerController = new Serializer($serializer);
+        $ignAttr = ['eleves', 'eleve', 'cours','instruments', 'interventions', 'typeInstruments', 'interPrets', 'user'];
+        return $serializerController->serializeObject($eleve, $ignAttr);
+//        return $this->render('eleve/ajouter.html.twig', [
+//            'form' => $form->createView(),
+//        ]);
     }
 
     public function modifierEleve(ManagerRegistry $doctrine, $id, Request $request){
